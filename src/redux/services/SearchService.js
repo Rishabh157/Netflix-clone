5e390cad7aa1f07ca8e2cf42bbd147df827580d6
rishabh157 <rishabhgour157@gmail.com>
2023-05-24 01:29:54 +0530

    remove a unused variable

--- src/redux/services/SearchService.js
+++ src/redux/services/SearchService.js
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL, API_KEY } from '../../constants/constants';
+import { BASE_URL } from '../../constants/constants';
 
 export const searchApi = createApi({
     reducerPath: 'search',
